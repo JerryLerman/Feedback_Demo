db94b59e355c16bcfcfe5efce3d9c5ae73f61f3b
Jerry Lerman <lerman@runbox.com>
2019-06-30 16:29:44 -0400

    favicon, changed bootstrap js, changed email provider

--- express/app.js
+++ express/app.js
@@ -49,7 +49,9 @@ var certFileBuf
 var mongooseOptions
     
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_SERVICE,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
